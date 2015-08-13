6378406f430f8321a0de638f84ab4d35694153b9
Stephen Lynn <stephen.g.lynn@gmail.com>
2015-08-13 05:26:46 -0600

    intial commit

---
+++ CustomViewControllerTransitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomViewControllerTransitions
+//
+//  Created by Stephen Lynn on 8/13/15.
+//  Copyright (c) 2015 FamilySearch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
